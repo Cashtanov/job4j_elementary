dc5c9afcd8659c048eb8798a8b6ef80cfa3369b1
github.com <Bkmzpett@gmsil.com>
2022-04-03 14:18:13 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

---
+++ src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FactorialTest {
+
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundredTwenty() {
+        int n = 5;
+        int rsl = Factorial.calc(n);
+        int expected = 120;
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        int n = 0;
+        int rsl = Factorial.calc(n);
+        int expected = 1;
+        Assert.assertEquals(expected, rsl);
+    }
+}
