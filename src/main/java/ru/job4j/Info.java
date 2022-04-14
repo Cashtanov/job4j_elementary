4658a73e8b635f951ff8afc79a07342c704cd759
Cashtanov <cashtanovis@yandex.ru>
2022-04-14 16:50:27 +0500

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("15.12.2021");
+        System.out.println("15.12.2022");
     }
 }
 
